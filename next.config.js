c56ef564292e1a9d4f0c6b4805434c83acc8056a
Naveendgp <naveendgp@gmail.com>
2024-10-09 20:18:57 +0530

    bug fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-export const output = "export";
-export const reactStrictMode = true;
-export const images = {
-  unoptimized: true, // Disable image optimization for static exports
+module.exports = {
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable image optimization for static exports
+  },
 };
